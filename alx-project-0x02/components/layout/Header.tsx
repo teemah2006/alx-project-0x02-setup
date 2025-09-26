2647ba4a56bfc5b4d4af4ae485a62ad4ad98e618
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-26 22:02:46 +0100

    task 2 complete

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,10 +1,17 @@
 'use client';
 import React from 'react';
-
+import Link from 'next/link';
 const Header: React.FC = () => {
   return (
     <header>
         <h1>Welcome to the Header Component</h1>
+        <nav>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/home">Home Page</Link></li>
+            </ul>
+        </nav>
     </header>
   );
 }
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function About() {
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  );
+}
