23902c615b7a6f6a64b86c58b55939996b497ca4
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-26 22:08:47 +0100

    task 3 complete

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,11 +1,13 @@
 'use client';
-import { CardProps } from "@/interfaces";
+import { type CardProps } from "@/interfaces";
 
-export const Card: React.FC<CardProps> = ({ title, content }) => {
+ const Card: React.FC<CardProps> = ({ title, content }) => {
     return(
         <div className="card">
             <h2>{title}</h2>
             <p>{content}</p>
         </div>
     );
-}       
+}   
+
+export default Card;
